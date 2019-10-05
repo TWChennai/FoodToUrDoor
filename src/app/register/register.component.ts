9d7c1a15f9afd60d492d00381e0a2b9e4138c527
Gowtham <gowthamraj198@gmail.com>
2019-10-05 16:58:37 +0530

    Gowtham: changed name to uppercase in restaurent page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,6 +14,7 @@ export class RegisterComponent implements OnInit {
     registerForm: FormGroup;
     submitted = false;
     success = false;
+    register = false;
 
     constructor(private formBuilder: FormBuilder, private dataService: DataserviceService,private router: Router) { }
 
