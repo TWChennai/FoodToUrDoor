6eeedfbc4410ad82e4be42cfb5f9905a63a25be7
Dileep B <dbellamk@thoughtworks.com>
2019-10-08 12:19:32 +0530

    Add remove item from cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -14,29 +14,35 @@ export class CartComponent implements OnInit {
 
   constructor() { }
 
+  getCartString() {
+    return localStorage.getItem('cart-items');
+  }
+
   ngOnInit() {
-    const cart = localStorage.getItem('cart-items').split(';');
-    this.order = new Ordermodel();
-    let restaurant = cart.find(x => x.startsWith('restaurant:'));
-    let restaurantInfo = restaurant.replace('restaurant:', '' ).split(':');
-    this.order.restaurantId = restaurantInfo[0];
-    this.order.restaurantName = restaurantInfo[1];
-    console.log(this.order.restaurantId);
-    this.order.orderItems = [];
-    let subTotal = 0;
-    this.order.deliveryFee = '50';
-    for ( let i = 1; i < cart.length - 1; i++ ) {
-        let itemDetails = cart[i].split(':');
-        let item = new OrderItem();
-        item.itemId = itemDetails[1];
-        item.itemName = itemDetails[2];
-        item.qty = itemDetails[4];
-        item.price = String(Number(itemDetails[3]) * Number(item.qty));
-        this.order.orderItems.push(item);
-        subTotal = subTotal + Number(item.price);
+    if( this.getCartString() != null ) {
+      const cart = this.getCartString().split(';');
+      this.order = new Ordermodel();
+      let restaurant = cart.find(x => x.startsWith('restaurant:'));
+      let restaurantInfo = restaurant.replace('restaurant:', '' ).split(':');
+      this.order.restaurantId = restaurantInfo[0];
+      this.order.restaurantName = restaurantInfo[1];
+      console.log(this.order.restaurantId);
+      this.order.orderItems = [];
+      let subTotal = 0;
+      this.order.deliveryFee = '50';
+      for ( let i = 1; i < cart.length - 1; i++ ) {
+          let itemDetails = cart[i].split(':');
+          let item = new OrderItem();
+          item.itemId = itemDetails[1];
+          item.itemName = itemDetails[2];
+          item.qty = itemDetails[4];
+          item.price = String(Number(itemDetails[3]) * Number(item.qty));
+          this.order.orderItems.push(item);
+          subTotal = subTotal + Number(item.price);
+      }
+      this.order.tax = String((subTotal * 5) / 100);
+      this.order.totalValue = String(subTotal + Number(this.order.tax) + Number(this.order.deliveryFee));
     }
-    this.order.tax = String((subTotal * 5) / 100);
-    this.order.totalValue = String(subTotal + Number(this.order.tax) + Number(this.order.deliveryFee));
   }
 
 }
--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -28,13 +28,20 @@ export class RestaurantComponent implements OnInit {
     return localStorage.getItem('cart-items');
   }
 
-  clearCartAndAddRestaurantIfRestaurantIsDifferent(restaurantId: string, restaurantName: string) {
+  doesCartContainsitemsFromRestaurant(restaurantId: string) {
     let cart = this.getCartInfo();
-    if ( cart != null && !cart.startsWith('restaurant:' + restaurantId )) {
+    if ( cart != null && cart.startsWith('restaurant:' + restaurantId )) {
+      return true;
+    }
+    return false;
+  }
+
+  clearCartAndAddRestaurantIfRestaurantIsDifferent(restaurantId: string, restaurantName: string) {
+    if ( !this.doesCartContainsitemsFromRestaurant( restaurantId )) {
       alert('Clearing Items from');
       localStorage.clear();
     }
-    cart = this.getCartInfo();
+    let cart = this.getCartInfo();
     if ( cart == null) {
       localStorage.setItem('cart-items', 'restaurant:' + restaurantId + ':' + restaurantName + ';');
     }
@@ -77,5 +84,39 @@ export class RestaurantComponent implements OnInit {
       localStorage.setItem('cart-items', cart + 'item:' + itemId + ':' + itemName + ':' + itemPrice + ':1;' );
       alert('Item added to cart!');
     }
+  }
+
+  decrementItemInCartByOne(itemId: string, itemName: string, itemPrice: string) {
+    let cart = this.getCartInfo().split(';');
+    const restaurant = cart[0];
+    for ( let i = 0; i < cart.length; i++ ) {
+      if ( cart[i].startsWith('item:' + itemId)) {
+        const itemCount = cart[i].split(':')[4];
+        console.log("before qty of item in cart :" + itemCount);
+        console.log("after qty of item in cart :" + (Number(itemCount) - 1 ));
+        cart[i] = 'item:' + itemId + ':' + itemName + ':' + itemPrice + ':' + (Number(itemCount) - 1 );
+      }
+    }
+    localStorage.setItem('cart-items', cart.join(';'));
+  }
+
+  removeFromCart(restaurantId: string, restaurantName: string, itemId: string, itemName: string, itemPrice: string) {
+    console.log('restaurant ID: ' + restaurantId)
+    console.log('in remove item from cart:' + this.doesCartContainsitemsFromRestaurant( restaurantId ));
+    if ( this.doesCartContainsitemsFromRestaurant( restaurantId )) {
+      console.log("cart contains items from restaurant");
+      if ( this.isItemAlreadyPresentInCart(itemId) ) {
+        console.log("cart contains item to -");
+        this.decrementItemInCartByOne(itemId, itemName, itemPrice);
+        alert('Item added to cart');
+      } else {
+        const cart = this.getCartInfo();
+        console.log('item is not in cart');
+        console.log('cart that is being set to:=' + cart + 'item:' + itemId + ':' + itemName + ':' + itemPrice + ':-1;' );
+        localStorage.setItem('cart-items', cart + 'item:' + itemId + ':' + itemName + ':' + itemPrice + ':-1;' );
+        alert('Item added to cart!');
+      }
+    }
+    // if item is not present 
   }
 }
