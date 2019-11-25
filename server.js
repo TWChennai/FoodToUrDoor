43d157333e6fd20f04f70adffd5b8f3bc9d2750f
Dileep B <dbellamk@thoughtworks.com>
2019-11-26 01:21:10 +0530

    removed unwanted headers added by proxy

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const app = express();
 
 app.get('/*', function(req,res) {
    res.header("Access-Control-Allow-Origin", '*');
-   res.header("Access-Control-Allow-Credentials", true);
-   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
    res.sendFile(path.join(__dirname+'/dist/FoodToUrDoor/index.html'));
 });
 
