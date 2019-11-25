e1ee40e64fb1dc8b264f0851fe2020a9cc0de779
Dileep B <dbellamk@thoughtworks.com>
2019-11-26 01:16:47 +0530

    moving cors config above

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/FoodToUrDoor'));
-
 app.get('/*', function(req,res) {
    res.header("Access-Control-Allow-Origin", '*');
    res.header("Access-Control-Allow-Credentials", true);
@@ -15,6 +12,9 @@ app.get('/*', function(req,res) {
    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
    res.sendFile(path.join(__dirname+'/dist/FoodToUrDoor/index.html'));
 });
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/FoodToUrDoor'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
