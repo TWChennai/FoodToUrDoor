ec270c6685af82be78077e65f72a4235ccb66a6f
Dileep B <dbellamk@thoughtworks.com>
2019-11-26 01:29:16 +0530

    add cors package

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 
 //Install express server
 const express = require('express');
+var cors = require('cors')
 const path = require('path');
 
 const app = express();
-
-app.get('/*', function(req,res) {
-   res.header("Access-Control-Allow-Origin", '*');
-   res.sendFile(path.join(__dirname+'/dist/FoodToUrDoor/index.html'));
-});
+app.use(cors())
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/FoodToUrDoor'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/FoodToUrDoor/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
